bd3561da7396c0680427c8c7617cd3a69aa8462a
Deepak G <deepakgunasekaran07@gmail.com>
2026-09-02 21:25:01 +0530

    Time: 1 ms (96.44%), Space: 43.8 MB (62.17%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.java
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.java
@@ -6,22 +6,15 @@ class Solution {
                 max=candies[i];
             }
         }
-        //boolean[] arr=new boolean[candies.length];
         List<Boolean> list = new ArrayList<>();
         for(int i=0;i<candies.length;i++){
             if(candies[i]+extraCandies >= max){
-                //arr[i]=true;
                 list.add(true);
             }
             else{
-                //arr[i]=false;
                 list.add(false);
             }
         }
-        // List<Boolean> list = new ArrayList<>();
-        // for (boolean value : arr) {
-        //     list.add(value);
-        // }
         return list;
     }
 }
