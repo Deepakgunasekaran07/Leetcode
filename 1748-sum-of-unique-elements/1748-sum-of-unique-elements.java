eb40a521b3b5411311e47c8c6644471f909d9535
Deepak G <deepakgunasekaran07@gmail.com>
2026-09-14 20:24:23 +0530

    Time: 2 ms (57.45%), Space: 42.9 MB (71.89%) - LeetHub

---
+++ 1748-sum-of-unique-elements/1748-sum-of-unique-elements.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int sumOfUnique(int[] nums) {
+        HashSet<Integer> set=new HashSet<>();
+        int sum=0;
+        for(int i=0;i<nums.length;i++){
+            int count=1;
+            for(int j=0;j<nums.length;j++){
+                if(nums[i]==nums[j]){
+                    count++;
+                }
+            }
+            if(count==2){
+                sum+=nums[i];
+            }
+        }
+        return sum;
+    }
+}
